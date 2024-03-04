f0093c956607c7b76f9b2c13efae534b5cc22ada
LilMystik <160438341+LilMystik@users.noreply.github.com>
2024-03-05 01:07:23 +0300

    Add files via upload

---
+++ src/main/java/table/tableapi/TableApi.java
@@ -0,0 +1,18 @@
+package table.tableapi;
+
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+@Component
+public class TableApi {
+    RestTemplate restTemplate;
+    public TableApi() {
+        restTemplate = new RestTemplate();
+    }
+    public String fetchGroupData(String groupId) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://iis.bsuir.by/api/v1/schedule?studentGroup=" + groupId);
+        return restTemplate.getForObject(builder.toUriString(), String.class);
+    }
+}
