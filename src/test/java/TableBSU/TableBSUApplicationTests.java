5ebba92a05fc64b8a6499606302d57829962234c
LilMystik <160438341+LilMystik@users.noreply.github.com>
2024-03-04 22:37:50 +0300

    Add files via upload

---
+++ src/test/java/TableBSU/TableBSUApplicationTests.java
@@ -0,0 +1,13 @@
+package TableBSU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableBSUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
